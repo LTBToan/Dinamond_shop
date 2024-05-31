7af8d513a07e1e1f3969df301e56a5817eef09d7
DANG NGUYEN <noplstks@gmail.com>
2024-05-31 15:44:19 +0700

    Pull

--- efurniture/src/pages/Product/ProductList.jsx
+++ efurniture/src/pages/Product/ProductList.jsx
@@ -21,9 +21,9 @@ export default function ProductList() {
       );
   };
 
-    useEffect(() => {
-        fetchCategoryData()
-    }, [])
+  useEffect(() => {
+    fetchCategoryData();
+  }, []);
 
   return (
     <>
